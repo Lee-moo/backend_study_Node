fcfd100600ba919847b480c9fba69bcb78148135
dlandif22 <dlandif22@gmail.com>
2020-12-04 00:10:00 +0900

    add

--- node 교과서/server5.js
+++ node 교과서/server5.js
@@ -36,6 +36,8 @@ http.createServer((req,res)=>{
             'Set-Cookie' : `session=${randonInt};Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
         });
         res.end();
+        //cookie.session이 있고 만료 기한을 넘기지 않았다면 session 변수에서 사용자 정보를 가져와서
+        //사용한다. 
     } else if (cookies.session && session[cookie.session].expires > new Date()){ 
         res.writeHead(200, {'Content-type' : 'text/html; charset=utf-8' });
         res.end(`${session[cookies.session].name}님 안녕하세요`);
@@ -48,3 +50,15 @@ http.createServer((req,res)=>{
 }).listen(8080, ()=>{
     console.log('port 8080 server on');
 });
+
+/*
+쿠키는 클라이언트에 key=value의 형식으로 저장하고 세션은 서버에 저장한다. 쿠키에 보안에 위협이 되는
+데이터를 저장하면 보안 문제가 발생할 수 있기 때문에 세션에 사용자 정보를 저장하고 쿠키에는 세션에
+randomInt같은 값을 전달해준다.
+
+물론 실제 배포용 서버에서는 세션을 위와 같이 변수에 저장하지 않는다. 서버가 멈추거나 재시작되면 메모리에
+저장된 변수가 초기화되기 때문이다. 또한, 서버의 메모리가 부족하면 세션을 저장하지 못하는 문제도 생긴다.
+그래서 보통은 데이터베이스에 넣어둔다. 
+
+REST란, "웹에 존재하는 모든 자원(이미지, 동영상, DB 자원)에 고유한 URI를 부여해 활용"
+*/
